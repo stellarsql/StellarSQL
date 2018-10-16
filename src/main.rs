1d6869a05857509528c7ba35da30eb0c4be18a34
tigercosmos <phy.tiger@gmail.com>
2018-10-16 22:11:55 +0800

    basic infrastructure: clap and dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate dotenv_codegen;
+
+use clap::App;
+
 fn main() {
     println!("Hello, StellarSQL!");
+
+    // Parse arguments
+    let yml = load_yaml!("../cli.yml");
+    let m = App::from_yaml(yml).get_matches();
+
+    let port = if let Some(port_) = m.value_of("port") {
+        port_
+    } else {
+        dotenv!("PORT")
+    };
+    println!("StellarSQL running on {} port", port);
 }
