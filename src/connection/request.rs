c48ac53cf4c04340e5f1f1946f90c0bbd64d4dcf
tigercosmos <phy.tiger@gmail.com>
2018-10-21 14:56:19 +0800

    request: enum Request and impl parse

---
+++ src/connection/request.rs
@@ -0,0 +1,11 @@
+use Response;
+
+pub enum Request {}
+
+impl Request {
+    pub fn parse(input: &str) -> Result<Response, String> {
+        Ok(Response::OK {
+            msg: input.to_string(),
+        })
+    }
+}
