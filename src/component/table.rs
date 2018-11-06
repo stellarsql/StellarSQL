c5d9d38b253d7bda9361ed916fc8a36c35e55d4c
tigercosmos <phy.tiger@gmail.com>
2018-11-07 00:30:04 +0800

    component: update `Table` struct, add `TableError`

--- src/component/table.rs
+++ src/component/table.rs
@@ -1,13 +1,56 @@
 use component::field::Field;
 use std::collections::HashMap;
+use std::fmt;
 
 #[derive(Debug, Clone)]
 pub struct Table {
-    name: String,
-    fields: HashMap<String, Field>,
-    primary_key: Vec<String>,
-    foreign_key: Vec<String>,
-    reference_table: Option<String>,
+    /* definition */
+    pub name: String,
+    pub fields: HashMap<String, Field>, // aka attributes
+    pub primary_key: Vec<String>,
+    pub foreign_key: Vec<String>,
+    pub reference_table: Option<String>,
+
+    /* value */
+    pub rows: Vec<Row>,
+
+    /* storage */
+    pub page: u64,           // which page of this table
+    pub cursors: (u64, u64), // cursors of range in a page
+}
+
+#[derive(Debug, Clone)]
+pub struct Row(HashMap<String, String>);
+
+impl Row {
+    fn new() -> Row {
+        Row(HashMap::new())
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum TableError {
+    InsertFieldNotExisted(String),
+    InsertFieldNotNullMismatched(String),
+    InsertFieldDefaultMismatched(String),
+}
+
+impl fmt::Display for TableError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            TableError::InsertFieldNotExisted(ref attr_name) => {
+                write!(f, "Insert Error: the table doesn't have `{}` attribute.", attr_name)
+            }
+            TableError::InsertFieldNotNullMismatched(ref attr_name) => {
+                write!(f, "Insert Error: {} could not be null", attr_name)
+            }
+            TableError::InsertFieldDefaultMismatched(ref attr_name) => write!(
+                f,
+                "Insert Error: {} has no default value. Need to declare the value.",
+                attr_name
+            ),
+        }
+    }
 }
 
 impl Table {
