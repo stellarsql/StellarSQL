86a2d6cc6e2391f9d7b5be85352e7c0b25cb1ec4
tigercosmos <phy.tiger@gmail.com>
2018-11-07 00:05:59 +0800

    component: remove `Value`

--- src/component/datatype.rs
+++ src/component/datatype.rs
@@ -1,17 +1,8 @@
 #[derive(Debug, Clone)]
 pub enum DataType {
-    Char,
+    Char(u8),
     Double,
     Float,
     Int,
-    Varchar,
-}
-
-#[derive(Debug, Clone)]
-pub enum Value {
-    Char(u8, String),
-    Double(f64),
-    Float(f32),
-    Int(i32),
-    Varchar(u8, String),
+    Varchar(u8),
 }
