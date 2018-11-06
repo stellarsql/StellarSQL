4deb17314bf3fceccf13e5883197670d8e6da1d3
tigercosmos <phy.tiger@gmail.com>
2018-11-07 00:05:17 +0800

    component: database.tables change to HashMap

--- src/component/database.rs
+++ src/component/database.rs
@@ -1,16 +1,17 @@
 use component::table::Table;
+use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 pub struct Database {
-    name: String,
-    tables: Vec<Table>,
+    pub name: String,
+    pub tables: HashMap<String, Table>,
 }
 
 impl Database {
-    fn new(name: &str) -> Database {
+    pub fn new(name: &str) -> Database {
         Database {
             name: name.to_string(),
-            tables: vec![],
+            tables: HashMap::new(),
         }
     }
 }
