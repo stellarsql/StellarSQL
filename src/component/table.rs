0868bae25c444d9b355fe9aa66250d7135a8d80d
tigercosmos <phy.tiger@gmail.com>
2018-11-08 00:34:59 +0800

    component: Table implement `insert_row`

--- src/component/table.rs
+++ src/component/table.rs
@@ -1,3 +1,5 @@
+use component::datatype::DataType;
+use component::field;
 use component::field::Field;
 use std::collections::HashMap;
 use std::fmt;
@@ -54,13 +56,124 @@ impl fmt::Display for TableError {
 }
 
 impl Table {
-    fn new(name: &str) -> Table {
+    pub fn new(name: &str) -> Table {
         Table {
             name: name.to_string(),
             fields: HashMap::new(),
+            rows: vec![],
             primary_key: vec![],
             foreign_key: vec![],
             reference_table: None,
+            page: 0,
+            cursors: (0, 0),
         }
+    }
+
+    /// `insert` row into the table
+    /// `key` and `value` are `&str`, and will be formated to the right type.
+    pub fn insert_row(&mut self, row: Vec<(&str, &str)>) -> Result<(), TableError> {
+        let mut new_row = Row::new();
+
+        // insert data into row
+        for (key, value) in row {
+            match self.fields.get(key) {
+                Some(field) => {
+                    if field.not_null && value == "null" {
+                        return Err(TableError::InsertFieldNotNullMismatched(field.clone().name));
+                    }
+                    new_row.0.insert(key.to_string(), value.to_string());
+                }
+                None => return Err(TableError::InsertFieldNotExisted(key.to_string())),
+            }
+        }
+
+        // check if the row fits the field
+        for (key, field) in self.fields.iter() {
+            match new_row.0.get(key) {
+                Some(_) => {}
+                None => {
+                    match field.clone().default {
+                        // if the attribute has default value, then insert with the default value.
+                        Some(value) => new_row.0.insert(key.to_string(), value.to_string()),
+                        None => return Err(TableError::InsertFieldDefaultMismatched(key.to_string())),
+                    };
+                }
+            };
+        }
+
+        self.rows.push(new_row);
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_insert_row() {
+        let mut table = Table::new("table_1");
+        table.fields.insert(
+            "attr_1".to_string(),
+            Field::new(
+                "attr_1",
+                DataType::Int,
+                true,                    // not_null is true
+                Some("123".to_string()), // default is 123
+                field::Checker::None,
+            ),
+        );
+        table.fields.insert(
+            "attr_2".to_string(),
+            Field::new(
+                "attr_2",
+                DataType::Int,
+                true, // not_null is true
+                None, // no default
+                field::Checker::None,
+            ),
+        );
+        table.fields.insert(
+            "attr_3".to_string(),
+            Field::new(
+                "attr_3",
+                DataType::Int,
+                false, // not null is false
+                None,  // no default
+                field::Checker::None,
+            ),
+        );
+
+        println!("correct data");
+        let data = vec![("attr_1", "123"), ("attr_2", "123"), ("attr_3", "123")];
+        assert!(table.insert_row(data).is_ok());
+
+        println!("`attr_2` is null while its not_null is true");
+        let data = vec![("attr_1", "123"), ("attr_2", "null"), ("attr_3", "123")];
+        assert!(table.insert_row(data).is_err());
+
+        println!("`attr_3` is null while its not_null is false");
+        let data = vec![("attr_1", "123"), ("attr_2", "123"), ("attr_3", "null")];
+        assert!(table.insert_row(data).is_ok());
+
+        println!("none given value `attr_2` while its default is None");
+        let data = vec![("attr_1", "123"), ("attr_3", "123")];
+        assert!(table.insert_row(data).is_err());
+
+        println!("none given value `attr_1` while it has default");
+        let data = vec![("attr_2", "123"), ("attr_3", "123")];
+        assert!(table.insert_row(data).is_ok());
+
+        println!("fields mismatched");
+        let data = vec![
+            ("attr_1", "123"),
+            ("attr_2", "123"),
+            ("attr_3", "123"),
+            ("attr_4", "123"),
+        ];
+        assert!(table.insert_row(data).is_err());
+        let data = vec![("attr_1", "123")];
+        assert!(table.insert_row(data).is_err());
     }
 }
