40b7ca54b1bcb53f0b27494fd513c082a2b96155
tigercosmos <phy.tiger@gmail.com>
2018-12-17 13:11:11 +0800

    component, datatype: add `get`

--- src/component/datatype.rs
+++ src/component/datatype.rs
@@ -1,8 +1,35 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum DataType {
     Char(u8),
     Double,
     Float,
     Int,
     Varchar(u8),
+}
+
+impl DataType {
+    pub fn get(s: &str, len: Option<u8>) -> Option<DataType> {
+        let length = len.unwrap_or(0);
+        let d = match s {
+            "char" => DataType::Char(length),
+            "double" => DataType::Double,
+            "float" => DataType::Float,
+            "int" => DataType::Int,
+            "varchar" => DataType::Varchar(length),
+            _ => return None,
+        };
+        Some(d)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_datatype() {
+        assert_eq!(DataType::Double, DataType::get("double", None).unwrap());
+        assert_eq!(DataType::Char(8), DataType::get("char", Some(8)).unwrap());
+        assert!(DataType::get("date", None).is_none());
+    }
 }
